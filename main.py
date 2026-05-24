58c63d78509284deb2a4793700a9d6211207401d
vinodpatil2002 <vinodnspatil@gmail.com>
2026-05-24 19:23:06 +0530

    Add post creation and response schemas with API endpoints for retrieving and creating posts

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from fastapi.exceptions import RequestValidationError
 from fastapi.responses import JSONResponse
 from starlette.exceptions import HTTPException as StarletteHTTPException
 from fastapi.templating import Jinja2Templates
+from schemas import PostCreate, PostResponse
 
 app = FastAPI()
 
@@ -55,12 +56,12 @@ def postPage(post_id: int, request: Request):
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Post not found")
 
 
-@app.get("/api/posts")
+@app.get("/api/posts", response_model=list[PostResponse])
 def getPosts():
     return posts
 
 
-@app.get("/api/posts/{post_id}")
+@app.get("/api/posts/{post_id}", response_model=PostResponse)
 def getPost(post_id: int):
     for post in posts:
         if post.get("id") == post_id:
@@ -112,3 +113,27 @@ def validation_exception_handler(request: Request, exception: RequestValidationE
         },
         status_code=status.HTTP_422_UNPROCESSABLE_CONTENT,
     )
+
+
+# create post
+
+
+@app.post(
+    "/api/posts",
+    response_model=PostCreate,
+    status_code=status.HTTP_201_CREATED,
+)
+def create_post(post: PostCreate):
+    new_id = max(p["id"] for p in posts) + 1 if posts else 1
+
+    new_post = {
+        "id": new_id,
+        "author": post.author,
+        "title": post.title,
+        "content": post.content,
+        "date_posted": "April 25, 2026",
+    }
+    posts.append(new_post)
+    return new_post
+
+
